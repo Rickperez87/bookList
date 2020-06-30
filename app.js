fea9f85690bbf3f245481afec1a74a38dc1b2e47
Rick Perez <Rickperez87@gmail.com>
2020-06-29 22:46:32 -0400

    Added delete button vanilla JS functionality

--- app.js
+++ app.js
@@ -26,12 +26,21 @@ class UI {
   static addBook(book) {
     let bookList = document.getElementById("book-list");
     let row = document.createElement("tr");
-    row.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.ibsn}</td><td><a href='#' class='btn btn-danger'>X</a></td>`;
+    row.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.ibsn}</td><td><a href='#' class='btn btn-danger delete'>X</a></td>`;
     bookList.appendChild(row);
   }
-  //submitBook
-  static submitBook()
+  // clear submit form
+  static clearSubmit() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#ibsn").value = "";
+  }
   // delete book from UI
+  static deleteBook(e) {
+    if (e.classList.contains("delete")) {
+      e.parentElement.parentElement.remove();
+    }
+  }
 }
 // class for storing books
 
@@ -42,7 +51,20 @@ addEventListener("DOMContentLoaded", () => {
   UI.displayBookList();
 });
 // submit book event
-
-// delete book event
-
+addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let ibsn = document.querySelector("#ibsn").value;
+  // create book object using constructor
+  const book = new Book(title, author, ibsn);
+  UI.addBook(book);
+  UI.clearSubmit();
+});
+// remove book event
+let list = document
+  .querySelector("#book-list")
+  .addEventListener("click", (e) => {
+    UI.deleteBook(e.target);
+  });
 // store book event
